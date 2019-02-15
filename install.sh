d24105b4b2f07d932de8dae616bd8a52b98ea036
Harry's Desktop <to000002@umn.edu>
2019-02-14 22:26:19 -0600

    excluded README from bootstrap I should have done this some time ago

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 rsync \
 		--exclude ".git/"\
 		--exclude "install.sh"\
+		--exclude "README.md"\
 		-avh . ~;
 git clone https://github.com/metalelf0/gnome-terminal-colors.git
 # note: you may have to copy .vim/bundle/vim-colors -> .vim/colors
